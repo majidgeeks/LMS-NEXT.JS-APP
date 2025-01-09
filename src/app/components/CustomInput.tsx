e89919091bb0506b3f5db2ddc88ca5d67e2688e7
Majid Hussain <majidrajput246@gmail.com>
2025-01-09 23:34:00 +0500

    sign up ui completed

--- src/app/components/CustomInput.tsx
+++ src/app/components/CustomInput.tsx
@@ -6,15 +6,18 @@ interface CustomInput{
     rightIcon? : ReactNode;
     placeholder? : string;
     type? : string;
+    onRightIconClick? : () => void;
 }
 
-const CustomInput : React.FC<CustomInput> = ({ label, className, rightIcon, placeholder, type }) => {
+const CustomInput : React.FC<CustomInput> = ({ label, className, rightIcon, placeholder, type, onRightIconClick  }) => {
     return (
         <div className="flex items-center w-full">
             <label htmlFor="" className="w-1/3 text-lg font-medium">{label}</label>
-            <div className="flex items-center bg-slate-200 px-2 rounded">
+            <div className="flex items-center bg-slate-200 px-2 rounded w-2/3">
                 <input type={type} placeholder={placeholder} className={`bg-slate-200 flex-grow outline-none, ${className}`} />
-                <button className="text-gray-500 cursor-pointer">{rightIcon}</button>
+                {rightIcon && (
+                    <button className="cursor-pointer" onClick={onRightIconClick}>{rightIcon}</button>
+                )}
 
             </div>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
-'client';
-import { FiEye } from "react-icons/fi";
+'use client';
+import { FiEye, FiEyeOff } from "react-icons/fi";
 import CustomInput from "./components/CustomInput";
+import Image from 'next/image'
+import { useState } from "react";
 
 
 export default function Home() {
+  const [eye, setEye] = useState(true);
+
+  const toggleEye = () => {
+    setEye((previous) => !previous)
+  };
+
   return (
-    <div className="w-full flex justify-evenly items-center bg-yellow-100">
-
-      <img
-        src="https://frontends.udemycdn.com/components/auth/desktop-illustration-x1.webp" alt="learning" width={500} />
-
-
-      <div className=" w-96 h-96 flex flex-col items-center">
+    <div className="w-screen h-screen flex justify-evenly items-center ">
+      <Image src={'/authentic-image.webp'}
+        alt="learning" width={500} height={200}
+      />
+      <div className=" w-96 flex flex-col items-center">
         <h1 className="mb-4 text-2xl font-semibold">Sign Up And Start Learning</h1>
         <div className="flex flex-col gap-4 w-full py-2">
           <CustomInput className={'outline-none'}
@@ -20,9 +26,27 @@ export default function Home() {
             label={'Last Name :'} />
           <CustomInput className={'outline-none'}
             label={'Email :'} />
+          <CustomInput className={'outline-none pr-1'}
+            label={'Password :'} onRightIconClick={toggleEye} rightIcon={!eye ? <FiEye /> : <FiEyeOff />} />
           <CustomInput className={'outline-none'}
             label={'Address :'} />
-            <label htmlFor="">Category</label>
+          <div className=" flex items-center gap-1">
+
+            <label htmlFor="" className="w-1/3 text-lg font-medium">Category :</label>
+            <select name="" id=""
+              className=" px-2 py-1 border rounded bg-slate-200 w-52"
+            >
+              <option value="">Engineering</option>
+              <option value="">Accounting</option>
+              <option value="">Photography</option>
+              <option value="">Fitness</option>
+              <option value="">Design</option>
+              <option value="">Ui Ux</option>
+            </select>
+          </div>
+          <div className="flex justify-center mt-3">
+            <button className="w-full py-1 bg-purple-500 text-white font-medium rounded-md">Submit</button>
+          </div>
         </div>
       </div>
     </div>
